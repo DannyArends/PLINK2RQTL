a290216d42efbb23e2a4ca9275928ec9e4652b64
DannyArends <danny.arends@gmail.com>
2014-07-25 16:01:55 +0200

    Updates for the PLINK converter, adding speed improvements and a fix for multiallelic markers

--- PLINKconverter.R
+++ PLINKconverter.R
@@ -16,13 +16,15 @@ PLINKtoCSVR <- function(ped = "test_complete.ped", map = "test.map", out = "cros
 
   SNPcolnames <- paste0(unlist(lapply(mapdata[,"ID"],rep,2)), c(".A",".B"))                                         # Create column names for the SNPs
 
-  peddata <- read.table(ped, colClasses=c("character"), na.strings=c("-9"))                                         # Read the PED data
+  peddata <-scan(ped, what=character(), na.strings=c("-9"))                                                         # Read the PED data
   columnNames <- NULL
   columnNames <- c(columnNames, "IID")
   if(!no.fid)     columnNames <- c(columnNames, "FID")
   if(!no.parents) columnNames <- c(columnNames, "PID", "MID")
   if(!no.sex)     columnNames <- c(columnNames, "Sex")
   if(!no.pheno)   columnNames <- c(columnNames, "Pheno")
+  peddata <- matrix(peddata, ncol=length(c(columnNames, SNPcolnames)), byrow = TRUE)
+
   colnames(peddata) <- c(columnNames, SNPcolnames)
 
   if(no.pheno) peddata <- cbind(peddata, Pheno=runif(nrow(peddata)))                                                # If there is no phenotype, create a random one
@@ -31,20 +33,29 @@ PLINKtoCSVR <- function(ped = "test_complete.ped", map = "test.map", out = "cros
   peddata[peddata[,"Sex"] == 1, "Sex"] <- "m"; peddata[peddata[,"Sex"] == 2, "Sex"] <- "f"                          # R/qtl uses m and f, for males and females
   genotypes <- matrix(NA, length(mapdata[,"ID"]), nrow(peddata))                                                    # Empty genotype matrix
   rownames(genotypes) <- mapdata[,"ID"]
+  column <- length(columnNames)+1
   for(snp in mapdata[,"ID"]){
-    cols <- grep(snp, colnames(peddata))                                                                            # Get the columns associated with this SNP
-    snpalleles <- sort(unique(unlist(peddata[,cols])))                                                              # The SNP alleles
+    cols <- column:(column+1)                                                                                       # Get the columns associated with this SNP
+    snpalleles <- sort(unique(unlist(as.character(peddata[,cols]))))                                                # The SNP alleles
     if(missing.genotype %in% snpalleles){
       snpalleles <- snpalleles[-which(snpalleles == missing.genotype)]                                              # Missing data should not count as an allele
     }
-    if(verbose) cat("For", snp,"found", snpalleles,"\n")                                                            # Some info / debug
-    genotype <- apply(peddata[,cols],1,function(x){
-      if(x[1] == missing.genotype) return(NA)                                                                       # Missing genotype data
-      if(x[1] != x[2]) return(2)                                                                                    # Hetrozygous *H*
-      if(x[1] == x[2] && x[1] == snpalleles[1]) return(1)                                                           # Homozygous Allele 1 *A*
-      if(x[1] == x[2] && x[1] == snpalleles[2]) return(3)                                                           # Homozygous Allele 2 *B*
-      stop("Should not get here, contact Danny.Arends@gmail.com")
-    })
+    if(length(snpalleles) > 2){
+      cat("[WARNING]", snp, "found multi allelic marker:", snpalleles, ", passed as all missing\n")
+      genotype <- rep(NA, nrow(peddata))
+    }else{
+      if(verbose){
+        cat((column - (length(columnNames)+1)) / 2,"/", length(mapdata[,"ID"]), snp,"found", snpalleles,"\n")       # Some info / debug
+      }
+      genotype <- apply(peddata[,cols], 1, function(x){
+        if(x[1] == missing.genotype) return(NA)                                                                     # Missing genotype data
+        if(x[1] != x[2]) return(2)                                                                                  # Heterozygous *H*
+        if(x[1] == x[2] && x[1] == snpalleles[1]) return(1)                                                         # Homozygous Allele 1 *A*
+        if(x[1] == x[2] && x[1] == snpalleles[2]) return(3)                                                         # Homozygous Allele 2 *B*
+        stop("Should not get here, contact Danny.Arends@gmail.com")
+      })
+    }
+    column <- column + 2
     genotypes[snp,] <- genotype
   }
 
